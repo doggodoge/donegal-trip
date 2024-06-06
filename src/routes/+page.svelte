462a58604bf0fd329664a565c1cd0ce19e2ef59a
Gary Moore <gary@garymoore.ie>
2024-06-06 15:14:28 +0100

    fix: sort out the home page a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
-<p>
-  This is a cycling trip from Castlerock, Co. Derry to Malin Head in the
-  Inishowen Peninsula, Co. Donegal.
-</p>
-
-<p>
-  There's a few things that needed planned out in a bit more detail, so made
-  this website to compile the info I've got so far.
-</p>
-
-<p>
-  Click on the links at the top, I'll keep this updated when anything changes.
-</p>
+<article>
+  <header><h2>Home</h2></header>
+  <p>
+    This is a cycling trip from Castlerock, Co. Derry to Malin Head in the
+    Inishowen Peninsula, Co. Donegal. There's a few things that needed planned
+    out in a bit more detail, so made this website to compile the info I've got
+    so far. Click on the links at the top, I'll keep this updated when anything
+    changes.
+  </p>
+</article>
