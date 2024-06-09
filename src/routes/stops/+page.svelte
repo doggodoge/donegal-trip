8d583cfcd9cc5bd41230cb3a92c0b4d2de2abf2f
Gary Moore <gary@garymoore.ie>
2024-06-09 18:15:57 +0100

    feat: add a few more stop locations

--- src/routes/stops/+page.svelte
+++ src/routes/stops/+page.svelte
@@ -38,5 +38,54 @@
     zoom={16}
   />
 
+  <h3>Pound Town</h3>
+
+  <p>
+    Mitchell wants me to stop here to get a photo with the Pound town road sign
+    that's there. Won't take too long 😂
+  </p>
+
+  <h4>Location</h4>
+
+  <Stop
+    location={{
+      lat: 55.2055,
+      lon: -6.97922,
+    }}
+    zoom={14}
+  />
+
+  <h3>Mc Laughlins</h3>
+
+  <p>
+    This is a wee shop we can stop at to get some things if we need them. We
+    need to go past this place anyway, handy in case we need something.
+  </p>
+
+  <h4>Location</h4>
+<!-- https://www.openstreetmap.org/?mlat=55.20451&mlon=-6.99083#map=18/55.20451/-6.99083&layers=C -->
+  <Stop
+    location={{
+      lat: 55.20451,
+      lon: -6.99083,
+    }}
+    zoom={16}
+  />
+
+  <h3>Magilligan Point View</h3>
+
+  <p>Pretty panoramic view of Magilligan Point in Co Derry.</p>
+
+  <h4>Location</h4>
+
+  <!-- https://www.openstreetmap.org/?mlat=55.22402&mlon=-7.01397#map=19/55.22402/-7.01397&layers=C -->
+  <Stop
+    location={{
+      lat: 55.22402,
+      lon: -7.01397,
+    }}
+    zoom={14}
+  />
+
   <p><em>More to come...</em></p>
 </article>
